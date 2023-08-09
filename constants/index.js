e035f7c6cb6544bb675d32851b7ef6065831ee0e
Payam <105141971+payam1374@users.noreply.github.com>
2023-08-09 12:02:48 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x7047caadd4909aaa840bd7c4d7d41d05f140732a";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
